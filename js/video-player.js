582ada01843ed00b211cd85be815849ab92e2622
Unknown <nigelbunner@gmail.com>
2018-04-27 13:42:08 +0100

    Sitemap fixes

--- js/video-player.js
+++ js/video-player.js
@@ -1,5 +1,5 @@
 videoUrl = str_replace("vid-", "", $_REQUEST['id']);
 title = $_REQUEST['title'];
 descrip = $_REQUEST['descrip'];
-echo "<iframe src=\"//www.youtube.com/embed/".$videoUrl."?autoplay=1\" width=\"280\" height=\"210\" frameborder=\"0\" allowfullscreen></iframe>";
+echo "<iframe src=\"https://www.youtube.com/embed/".$videoUrl."?autoplay=1\" width=\"280\" height=\"210\" frameborder=\"0\" allowfullscreen></iframe>";
 echo "<p>".$title." &mdash; <em>".$descrip."</em></p>";
